b61f929bfb3b0a9db2c89d468aba03a80466b493
Alejandro Torres <alejandro123888@hotmail.com>
2016-07-25 15:01:14 +0200

    Header.php creado

---
+++ header.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>Hola esto es un comentario en un archivo php</p>";
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php include 'header.php';?>
+    
+    asd
+<?php
+echo "My first PHP script!";
+?>
+</body>
+</html>
